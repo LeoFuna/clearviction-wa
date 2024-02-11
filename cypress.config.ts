fd8451c4d4b68ccf58f33c3e8b81b4fc9a7a5453
leofuna <leo.funa@gmail.com>
2024-02-11 07:05:02 -0300

    test: open calculator from home

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000',
+    // execTimeout: 6000,
+  },
+});
