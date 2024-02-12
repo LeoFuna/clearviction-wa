848d3deca1bfdc20639738a9890eee9426828167
leofuna <leo.funa@gmail.com>
2024-02-12 10:24:12 -0300

    feat: increase exec timeout for test

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
       // implement node event listeners here
     },
     baseUrl: 'http://localhost:3000',
-    execTimeout: 10000,
+    execTimeout: 15000,
   },
 });
